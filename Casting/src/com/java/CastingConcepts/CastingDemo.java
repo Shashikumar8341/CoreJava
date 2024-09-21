d67171a719c6d9acc253b9fe04fb044368ea3c22
shashikumar <shashi.b8341@gmail.com>
2024-09-21 16:58:08 +0530

    Add files via upload

---
+++ Casting/src/com/java/CastingConcepts/CastingDemo.java
@@ -0,0 +1,19 @@
+package com.java.CastingConcepts;
+
+public class CastingDemo {
+	public static void main(String[] args) {
+//type casting
+		long d=8341866042L;
+		double f=2564.00D;
+		int h=1;
+		short k=26486;
+		float number=586.0f;
+
+		System.out.println((int)f);  
+		System.out.println((double)d);
+		System.out.println((short)number);
+		System.out.println((float)k);
+		System.out.println((long)k);
+		System.out.println((double)h);
+	}
+}
