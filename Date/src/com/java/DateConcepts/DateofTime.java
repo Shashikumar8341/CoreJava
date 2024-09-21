9c1b82e329eeba9e72ef74ec7bdec4428ca7a84c
shashikumar <shashi.b8341@gmail.com>
2024-09-21 16:56:12 +0530

    Add files via upload

---
+++ Date/src/com/java/DateConcepts/DateofTime.java
@@ -0,0 +1,13 @@
+package com.java.DateConcepts;
+
+import java.util.Date;
+
+public class DateofTime{
+	String date;
+	
+		public static  void main (String[]args) {
+//		String Date = "27/01/20000";
+		Date d1=new Date();
+//		System.out.println("dd/mm/yyyy:"+Date);
+		System.out.println(d1);
+}}
